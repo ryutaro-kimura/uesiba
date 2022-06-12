059558207b629acf43a90422a211db2a9e9f4e02
ryutaro-kimura <sasuker2264@gmail.com>
2022-06-12 22:40:51 +0900

    add: Custom Visionに推論させる関数の追加

--- api/CognitiveTriger/index.js
+++ api/CognitiveTriger/index.js
@@ -1,5 +1,7 @@
 const line = require('@line/bot-sdk');
 const fs = require('fs');
+const PredictionApi = require("@azure/cognitiveservices-customvision-prediction");
+const msRest = require("@azure/ms-rest-js");
 require('dotenv').config()
 
 const env = process.env
@@ -19,17 +21,16 @@ module.exports = async function (context, req) {
             // 送信された画像を取得する処理
             const downloadPath = 'Image/qr.png';
             let savedPath = await downloadContent(event.message.id, downloadPath);
-            // await sample(event.message.id, downloadPath)
             // 取得した画像をCognitiveに投げて、検知結果を貰う処理
-
+            const phrase = await getPredictionsPhrase(downloadPath)
             // 検知結果をリプライする処理
 
             var message = {
                 type: 'text',
-                text: `${savedPath}として保存しました。`
+                text: phrase
             }
-            if (req.body.events[0].replyToken) {
-                client.replyMessage(req.body.events[0].replyToken, message);
+            if (event.replyToken) {
+                client.replyMessage(event.replyToken, message);
             }
         }
         else {
@@ -60,18 +61,28 @@ function downloadContent(messageId, downloadPath) {
     }));
 }
 
-function sample(messageId, downloadPath) {
-    client.getMessageContent(messageId)
-    .then((stream) => {
-        const dest = fs.createWriteStream(downloadPath);
-        stream.on('data', (chunk) => {
-          dest.write(chunk)
+async function getPredictionsPhrase(downloadPath) {
+    const predictor_credentials = new msRest.ApiKeyCredentials({ inHeader: { "Prediction-key": env.PREDICTION_KEY } });
+    const predictionEndpoint = env.PREDICTION_ENDPOINT
+    const predictor = new PredictionApi.PredictionAPIClient(predictor_credentials, predictionEndpoint);
+
+    const testFile = fs.readFileSync(downloadPath);
+    let resultText = '';
+    try {
+        const results = await predictor.classifyImage(env.ITERATION_ID, 'Iteration1', testFile);
+
+        // Show results
+        console.log("Results:");
+        results.predictions.forEach(predictedResult => {
+            console.log(`\t ${predictedResult.tagName}: ${(predictedResult.probability * 100.0).toFixed(2)}%`);
+            resultText += `\n ${predictedResult.tagName}: ${(predictedResult.probability * 100.0).toFixed(2)}%`
         });
-        stream.on('error', (err) => {
-        });
-        stream.on('end', () => {
-            const url = fs.readFileSync(downloadPath, { encoding: "base64" });
-            return url
-        });
-      });
+        console.log(resultText);
+        resultText = "わしが思うに..." + resultText + "\nじゃな。"
+        return resultText;
+    } catch (err) {
+        console.error('hogehoge',err.message)
+        resultText = "なんじゃろ...最近目が悪くてのぉ...別の画像で試してくれぃ"
+        return resultText;
+    }
 }
